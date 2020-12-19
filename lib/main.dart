a0a9cf4a35fcc70944c9ab0d0e631f5903220eac
deadshot674gam <sunnyt0225@gmail.com>
2020-12-19 22:40:46 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,62 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 110.0,
+                  ),
+                  CircleAvatar(
+                      radius: 40.0,
+                      backgroundImage: AssetImage("images/divyanshu.png")
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                    child: Text(
+                      "Divyanshu Tiwari",
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+                    child: Text(
+                      "FLUTTER DEVELOPER",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
