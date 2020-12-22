11900cc36b08060dc3c99a91bb9069e0faf43ec8
deadshot674gam <sunnyt0225@gmail.com>
2020-12-22 18:02:55 +0530

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,42 +14,86 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
-            mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Column(
-                children: <Widget>[
+                children: [
                   SizedBox(
-                    height: 110.0,
+                    height: 75.0,
                   ),
                   CircleAvatar(
-                      radius: 40.0,
-                      backgroundImage: AssetImage("images/divyanshu.png")
+                    backgroundImage: AssetImage('images/divyanshu.png'),
+                    radius: 50.0,
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
                     child: Text(
                       "Divyanshu Tiwari",
                       style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        color: Colors.white,
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                          color: Colors.white,
+                          fontFamily: 'Pacifico',
+                          fontSize: 35.0),
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
                     child: Text(
                       "FLUTTER DEVELOPER",
                       style: TextStyle(
-                        fontFamily: "SourceSansPro",
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w300,
-                        color: Colors.white,
-                      ),
+                          color: Colors.white,
+                          fontFamily: 'SourceSanPro',
+                          letterSpacing: 3.5,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w300),
                     ),
-                  )
+                  ),
+                  Card(
+                      color: Colors.white,
+                      margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 5.0),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(10.0, 7.0, 175.0, 7.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.call,
+                              color: Colors.teal,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              "+91 9454900674",
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.teal.shade900,
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                  Card(
+                      color: Colors.white,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(10.0, 7.0, 130.0, 7.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.email,
+                              color: Colors.teal,
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text(
+                              "sunnyt0225@gmail.com",
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.teal.shade900,
+                              ),
+                            )
+                          ],
+                        ),
+                      ))
                 ],
               )
             ],
@@ -59,4 +103,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
